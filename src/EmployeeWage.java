548fa546360a4092e1c9c51a6cf4822379725370
raju dey <rajudey432@gmail.com>
2023-04-09 13:35:47 +0530

    added : employee attendence

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,6 +3,14 @@
 public class EmployeeWage {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to employeewage");
+      //  System.out.println("Welcome to employeewage");
+
+        int isPresent = (int)(Math.random() * 10) % 2;
+       // System.out.println(isPresent);
+        if (isPresent == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
