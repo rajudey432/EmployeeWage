65c7aaca185523c9093820e9906e10941598c957
raju dey <rajudey432@gmail.com>
2023-04-09 14:10:06 +0530

    added : part time employeewage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -6,18 +6,27 @@ public class EmployeeWage {
 
     static final int FULL_DAY_HOUR = 8;
 
-    public static void main(String[] args) {
-      //  System.out.println("Welcome to employeewage");
+    static final int PART_TIME_HOUR = 4;
 
-        int isPresent = (int)(Math.random() * 10) % 2;
+
+
+    public static void main(String[] args) {
+       System.out.println("Welcome to employeewage");
+
+        int empCheck = (int)(Math.random() * 10) % 2;
         int dailyWage = 0;
-       // System.out.println(isPresent);
-        if (isPresent == 1) {
+        if (empCheck == 2) {
             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            System.out.println("Employee is Present");
-        } else {
-            System.out.println("Employee is Absent");
-        }
+            System.out.println("Employee is Full time");
+        } else if (empCheck == 1) {
+            dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+            System.out.println("Employee is Part time");
+        }   else {
+              System.out.println("Employee is Absent");
+    }
         System.out.println("Daily wage => " + dailyWage);
+       // System.out.println(isPresent);
+
+
     }
 }
